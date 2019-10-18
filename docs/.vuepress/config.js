d04fee2e26bc22bf8724230d08c1bc10815aad86
DUONGCHIBINH <duongchibinh97@gmail.com>
2019-10-18 18:49:46 +0700

    yarn

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Hello VuePress1',
+    description: 'DuongChiBinh',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' }
+        ]
+      }
+};
+
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+# Hello VuePress123
+
+
+## h2
+
+chữ gì đó
+
+[Link somewhere](google.com)
