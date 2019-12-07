0bf076ff47825f0d07e3dccfd9abaee2ee145347
DUONGCHIBINH <duongchibinh97@gmail.com>
2019-12-07 21:21:00 +0700

    --ADD USER TO DATA BY API

--- client/src/pages/admin/ad_user.vue
+++ client/src/pages/admin/ad_user.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-data-table :headers="headers" :items="desserts"  class="elevation-1">
+  <v-data-table :headers="headers" :items="desserts" class="elevation-1">
     <template v-slot:top>
       <v-toolbar flat color="white">
         <v-toolbar-title>Danh mục người dùng</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-divider class="mx-6" inset vertical></v-divider>
         <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
+        <v-dialog v-model="dialog" max-width="800px">
           <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+            <v-btn color="primary" dark class="mb-2" v-on="on">Thêm mới</v-btn>
           </template>
           <v-card>
             <v-card-title>
@@ -17,20 +17,17 @@
             <v-card-text>
               <v-container>
                 <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                  <v-col cols="12" sm="6" md="6">
+                    <v-text-field v-model="editedItem.userid" label="ID"></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
+                  <v-col cols="12" sm="6" md="6">
+                    <v-text-field v-model="editedItem.username" label="Name"></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
+                  <v-col cols="12" sm="6" md="6">
+                    <v-text-field v-model="editedItem.userpass" label="Pass"></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
+                  <v-col cols="12" sm="6" md="6">
+                    <v-combobox v-model="editedItem.type" :items="user_types" label="Loại user"></v-combobox>
                   </v-col>
                 </v-row>
               </v-container>
@@ -46,7 +43,6 @@
       </v-toolbar>
     </template>
 
-    
     <template v-slot:item.action="{ item }">
       <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
       <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
@@ -61,10 +57,10 @@ import axios from "axios";
 
 export default {
   data: () => ({
-    users:[],
+    users: [],
     dialog: false,
     headers: [
-        {
+      {
         text: "ID",
         align: "left",
         value: "_id"
@@ -73,40 +69,36 @@ export default {
         text: "User ID",
         align: "left",
         value: "userid"
-      }, 
+      },
       { text: "User Name", value: "username" },
       {
         text: "Type",
         align: "left",
         value: "type"
       },
-           { text: "Actions", value: "action" },
-           
-
+      { text: "Actions", value: "action" }
     ],
     desserts: [],
     editedIndex: -1,
     editedItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0
+      userid: "",
+      username: "",
+      userpass: "",
+      type: ""
     },
     defaultItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0
-    }
+      userid: "",
+      username: "",
+      userpass: "",
+      type: ""
+    },
+    user_types: ["Admin", "User", "PB", "PG", "Quản lý", "Nhà tuyển dụng"]
   }),
 
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";  
-    },
-      
+      return this.editedIndex === -1 ? "Thêm mới" : "Cập nhật";
+    }
   },
 
   watch: {
@@ -117,26 +109,23 @@ export default {
 
   created() {
     this.initialize();
- 
   },
- mounted() {
+  mounted() {
     this.reload();
   },
   methods: {
-    reload(){
+    reload() {
       axios
         .get(`http://localhost:5000/api/user`)
         .then(response => {
           this.users = response.data;
-          this.desserts =  response.data.data; 
+          this.desserts = response.data.data;
         })
         .catch(e => {
           this.errors.push(e);
         });
     },
-    initialize() {
-     
-    },
+    initialize() {},
 
     editItem(item) {
       this.editedIndex = this.desserts.indexOf(item);
@@ -146,7 +135,7 @@ export default {
 
     deleteItem(item) {
       const index = this.desserts.indexOf(item);
-      if(confirm("Bạn có chắc muốn xóa dòng này?") )
+      if (confirm("Bạn có chắc muốn xóa dòng này?"))
         this.desserts.splice(index, 1);
     },
 
@@ -155,16 +144,46 @@ export default {
       setTimeout(() => {
         this.editedItem = Object.assign({}, this.defaultItem);
         this.editedIndex = -1;
-      }, 300);
+      }, 600);
     },
 
-    save() {
+    async save() {
       if (this.editedIndex > -1) {
         Object.assign(this.desserts[this.editedIndex], this.editedItem);
       } else {
         this.desserts.push(this.editedItem);
       }
-      this.close();
+      this.themmoi();
+         this.close();
+    
+    },
+    themmoi() {
+      console.log("----------------------------------------------------");
+      if (this.editedItem.userid == '') {
+        console.log("userid null");
+        return;
+      }
+      console.log(this.editedItem);
+      axios
+        .post(`http://localhost:5000/api/user/`, this.editedItem, {
+          headers: {
+            "content-type": "application/json"
+          }
+        })
+        .then(response => {
+          console.log(response.data.confirmation);
+          if(response.data.confirmation =='add success')
+          {
+            alert('Thêm thành công!');
+            return true;
+          }
+          else {alert('Thêm KHÔNG thành công!');return false;}
+        })
+        .catch(e => {
+          alert('Thêm KHÔNG thành công!');
+          console.log(e);
+          return false;
+        });
     }
   }
 };
