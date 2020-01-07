366d77c37a454a651e59746b45c0516e5c27480e
Bình <Duongchibinh97@gmail.com>
2020-01-07 18:05:44 +0700

    momo api

--- server/routes/api/momo.js
+++ server/routes/api/momo.js
@@ -1,42 +1,65 @@
 const express = require('express');
 const router = express.Router();
 
-
-
+const axios = require('axios')
+var CryptoJS = require("crypto-js");
 //Get GET     find: api/user?username=Binh  
 router.get('/', async(req, res) => {
 
+        const query = req.query;
+
+        var mess = 'requestId=test01&amount=99999&orderId=test01&orderInfo=testinfo&returnUrl=https://momo.vn&notifyUrl=https://momo.vn&extraData=name=binh'
+        var key = '7OFeEEMd6JZj7xIXndDQw41zie2em3gZ'
+            //   res.send(CryptoJS.HmacSHA256(mess, key).toString());
+        let taochuky = {
+            'partnerCode': process.env.partnerCode,
+            'accessKey': process.env.accessKey,
+            'requestId': req.query.requestId,
+            'amount': req.query.amount,
+            'orderId': req.query.orderId,
+            'orderInfo': req.query.orderInfo,
+            'returnUrl': 'https://momo.vn',
+            'notifyUrl': 'https://momo.vn',
+            'extraData': req.query.extraData,
+        };
+        let chukypara = Object.entries(taochuky).map(e => e.join('=')).join('&');
 
 
-    const query = req.query;
+        CryptoJS.HmacSHA256(chukypara, process.env.secretKey).toString()
 
-    const m_partnerCode = query.partnerCode;
-    const m_accessKey = query.accessKey;
-    const m_requestId = query.requestId;
-    const m_orderId = query.orderId;
-    const m_requestType = query.requestType;
-    const m_amount = query.amount;
-    const m_transId = query.transId;
-    const m_payType = query.payType;
-    const m_errorCode = query.errorCode;
-    const m_message = query.message;
-    const m_localMessage = query.localMessage;
-    const m_extraData = query.extraData;
-    const m_signature = query.signature;
-    const m_orderInfo = query.orderInfo;
+        let myParams = {
+            'partnerCode': process.env.partnerCode,
+            'accessKey': process.env.accessKey,
+            'requestId': req.query.requestId,
+            'amount': req.query.amount,
+            'orderId': req.query.orderId,
+            'extraData': req.query.extraData,
+            'orderInfo': req.query.orderInfo,
+            'returnUrl': 'https://momo.vn',
+            'notifyUrl': 'https://momo.vn',
+            'signature': CryptoJS.HmacSHA256(chukypara, process.env.secretKey).toString(),
+            'requestType': 'captureMoMoWallet',
+        };
+        let u = Object.entries(myParams).map(e => e.join('=')).join('&');
+        // res.json({ url: myParams });
 
-    res.json({
-        "confirmation": 'success',
-        data: {
-            "orderId": m_orderId,
-            "requestType": m_requestType,
-            "amount": m_amount,
-            "transId": m_transId,
-            "payType": m_payType,
-            "orderInfo": m_orderInfo
-        }
-    });
+        axios
+            .post(`https://test-payment.momo.vn/gw_payment/transactionProcessor`, myParams, {
+                headers: {
+                    "content-type": "application/json"
+                }
+            })
+            .then((response) => {
+                var temp = response.data;
+                res.json(temp)
 
-});
+            })
+            .catch((error) => {
+                res.json(error)
+            })
+
+    }
+
+);
 
 module.exports = router;
